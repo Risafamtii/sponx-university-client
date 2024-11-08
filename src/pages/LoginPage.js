358dcc5a89c69f23ee2c9a42ca532ef82dfd1cb7
NadhiyaNashath <nashathnadhiya@gmail.com>
2024-11-08 13:32:37 +0530

    SPON-[56]-configure-flow-bite

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+  <div>
+    Login Page
+  </div>
+
+  );
 };
 
 export default LoginPage;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const flowbite = require("flowbite-react/tailwind");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    flowbite.content(),
+  ],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
+};
