ba3450983f6198400a01aa9bb51d78d332a285c7
AthharPro <athharasif2002@gmail.com>
2024-12-04 10:23:39 +0530

    [config] - React + Vite + Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,8 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        customBlue: '#1DA1F2',
-        customWhite: "rgba(255,255,255,0.18)"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
