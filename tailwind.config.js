35a0d6ae933fced9a8a67d0954100fcbf82cc7be
Amrah444 <ammuslamath@gmail.com>
2024-12-09 08:51:25 +0530

    conflict ressolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,21 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(), // Add Flowbite content
   ],
   theme: {
     extend: {
       colors: {
         customBlue: '#1DA1F2',
-        customWhite: "rgba(255,255,255,0.18)"
-      }
+        customWhite: "rgba(255,255,255,0.18)",
+      },
     },
   },
-  plugins: [],
-}
-=======
-const flowbite = require("flowbite-react/tailwind");
-
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    // ...
-    flowbite.content(),
-  ],
   plugins: [
-    // ...
-    flowbite.plugin(),
+    flowbite.plugin(), // Include Flowbite plugin
   ],
 };
->>>>>>> 58a907f29f4cb9c0c245fd314f918fd6d76a45dd
