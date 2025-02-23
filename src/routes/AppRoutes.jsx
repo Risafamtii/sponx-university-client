152a9e5a46ba3aa8ba82538c3b9014f70312511a
Amrah444 <slamathamrah@gmail.com>
2025-02-23 20:33:04 +0530

    alignment

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -26,6 +26,7 @@ import ClubProfile from '../pages/Club/Profile'
 import ClubSelectAd from '../pages/Club/SelectedAd'
 import ClubAdvertise from '../pages/Club/AdvertiseEvents'
 import ClubEventDetails from '../pages/Club/EventDetails'
+import ClubCreateEvent from '../pages/Club/CreateEvent'
 
 import CompanyLayout from '../layouts/CompanyLayout';
 import CompanyOverview from '../pages/Company/Overview';
@@ -74,6 +75,7 @@ const AppRoutes = () => {
         <Route path ="bankdetails" element ={<ClubBank/>}/>
         <Route path ="clubdetails" element ={<ClubDetails/>}/>
         <Route path ="myevents" element ={<ClubEvents/>}/>
+        <Route path ="createevents" element ={<ClubCreateEvent/>}/>
         <Route path ="eventdetails" element ={<ClubEventDetails/>}/>
         <Route path ="profile" element ={<ClubProfile/>}/>
         <Route path ="selectad" element ={<ClubSelectAd/>}/>
